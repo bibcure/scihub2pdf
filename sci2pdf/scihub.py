f6a3d0f1d04badabd128836b62cf6af743cbf3f2
bruno_messias <messias.physics@gmail.com>
2017-08-22 09:39:44 -0300

    Show captcha image

--- sci2pdf/scihub.py
+++ sci2pdf/scihub.py
@@ -4,21 +4,19 @@ from lxml import html
 from multiprocessing.dummy import Pool as ThreadPool
 from title2bib.crossref import get_bib_from_title
 import bibtexparser
-import pdb
-# from PIL import Image
 from builtins import input
-# from StringIO import StringIO
-# from builtins import input
-# try:
-    # from urllib import quote
-# except ImportError:
-    # from urllib.parse import quote
-
+from PIL import Image
+from StringIO import StringIO
+import pdb
 
 headers = {
     # "Connection": "close",
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 }
+
+
+url_captcha_scihub = "http://moscow.sci-hub.cc"
+url_scihub = "http://libgen.io/scimag/ads.php"
 
 
 def norm_url(url):
@@ -32,10 +30,9 @@ def get_libgen_url(bib, s):
     download_link = None
     doi = bib["doi"]
     print(doi)
-    url = "http://libgen.io/scimag/ads.php"
     params = {"doi": doi, "downloadname": ""}
 
-    r = s.get(url, params=params, headers=headers, allow_redirects=True)
+    r = s.get(url_scihub, params=params, headers=headers, allow_redirects=True)
     if r.status_code == 200:
         html_tree = html.fromstring(r.content)
         html_a = html_tree.xpath("/html/body/table/tr/td[3]/a")
@@ -50,14 +47,15 @@ def get_libgen_url(bib, s):
     return bib
 
 
-def check_captcha(html_tree, url, s):
+def check_captcha(iframe_url, url, html_tree, s):
     html_captcha = html_tree.xpath("//*[@id='captcha']")
     if len(html_captcha) > 0:
+        captcha_url = url_captcha_scihub+html_captcha[0].attrib["src"]
+        r = s.get(captcha_url)
+
+        Image.open(StringIO(r.content)).show()
+        # captcha_solution = input("Put the CAPTCHA: ")
         pdb.set_trace()
-        captcha_url = url+html_captcha[0].attrib["src"]
-        s.get(captcha_url)
-        # Image.open(StringIO(r.content)).show()
-        captcha_solution = input("Put the CAPTCHA: ")
         # submit_captcha(captcha_solution)
         return True
     else:
@@ -78,7 +76,7 @@ def get_scihub_url(bib, s):
         iframe_url = html_tree.xpath("//iframe/@src")[0]
         ri = s.get(iframe_url, headers=headers)
         html_tree_ri = html.fromstring(ri.content)
-        if check_captcha(html_tree_ri, url, s) is False:
+        if check_captcha(iframe_url, url,  html_tree_ri, s) is False:
             # get_scihub_url(bib,)
             html_pdf = html_tree.xpath("//*[@id='pdf']")
             if len(html_pdf) > 0:
