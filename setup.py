1b03b0207ebe88425d0461e06b0327bcf5269558
bruno_messias <messias.physics@gmail.com>
2017-08-24 15:58:00 -0300

    The string arxiv will be removed by arxivcheck module

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
 
-readme = open('README','r')
+readme = open('README', 'r')
 README_TEXT = readme.read()
 readme.close()
 
 setup(
     name="scihub2pdf",
-    version="0.3.0",
-    packages = find_packages(exclude=["build",]),
+    version="0.3.1",
+    packages=find_packages(exclude=["build", ]),
     scripts=["scihub2pdf/bin/scihub2pdf"],
-    long_description = README_TEXT,
-    install_requires = ["bibtexparser",
-                        "title2bib",
-                        "arxivcheck",
-                        "future",
-                        "Pillow",
-                        "requests",
-                        "lxml"],
+    long_description=README_TEXT,
+    install_requires=["bibtexparser",
+                      "title2bib",
+                      "arxivcheck",
+                      "future",
+                      "Pillow",
+                      "requests",
+                      "lxml"],
     include_package_data=True,
     license="GPLv3",
     description="Downloads pdfs via a DOI number(or arxivId), article title or a bibtex file, sci-hub",
     author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/bibcure/scihub2pdf/archive/0.3.0.tar.gz",
-    keywords=["bibtex", "sci-hub", "libgen", "doi",  "science","scientific-journals"],
+    download_url="https://github.com/bibcure/scihub2pdf/archive/0.3.1.tar.gz",
+    keywords=["bibtex", "sci-hub", "libgen", "doi",  "science", "scientific-journals"],
 
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
